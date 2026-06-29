81c2800007ca97630620e38790067346c1d2bfac
Hunter X <79095757+gitcuaquan@users.noreply.github.com>
2026-06-29 21:16:08 +0700

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -5,7 +5,7 @@ useHead({
   meta: [
     {
       name: "viewport",
-      content: "width=device-width, initial-scale=1,max-scale=1",
+      content: "width=device-width, initial-scale=1",
     },
   ],
   link: [
