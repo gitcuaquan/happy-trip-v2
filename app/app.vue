5d28921de6469f4fffc9a1ea8aae2cd3bccf692d
Hunter X <79095757+gitcuaquan@users.noreply.github.com>
2026-07-24 21:29:15 +0700

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -63,13 +63,13 @@ useHead({
   script: [
     {
       key: 'meta-pixel',
-      innerHTML: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1160626542496232');fbq('track','PageView');`,
+      innerHTML: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1160626542496232');fbq('init','2229315357857572');fbq('track','PageView');`,
     },
   ],
   noscript: [
     {
       key: 'meta-pixel-noscript',
-      innerHTML: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1160626542496232&ev=PageView&noscript=1" />`,
+      innerHTML: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1160626542496232&ev=PageView&noscript=1" /><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2229315357857572&ev=PageView&noscript=1" />`,
     },
   ],
 });
